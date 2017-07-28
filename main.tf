a800d0e4cabab2cd2639acdfdcf7cccb105d596e
Tom Yandell <tom@tomyandell.co>
2017-07-28 08:32:06 +0000

    Terraform format
    JIRA: PLAT-1072

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ module "vpc" {
 
   name = "${var.name_prefix}-vpc"
 
-  cidr = "${var.vpc_cidr}"
+  cidr            = "${var.vpc_cidr}"
   private_subnets = ["${var.private_subnet_cidrs}"]
   public_subnets  = ["${var.public_subnet_cidrs}"]
 
-  enable_nat_gateway = "true"
-  enable_dns_support = "true"
+  enable_nat_gateway   = "true"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
 
-  azs      = ["${data.aws_availability_zones.available.names}"]
+  azs = ["${data.aws_availability_zones.available.names}"]
 }
 
 module "vpn" {
@@ -26,27 +26,27 @@ module "vpn" {
 module "leg_a_customer_gateway" {
   source = "github.com/mergermarket/tf_aws_customer_gw"
 
-  name = "${var.name_prefix}-${var.leg_a_name}"
-  vpn_gateway_id = "${module.vpn.vgw_id}"
-  ip_address = "${var.leg_a_ip_address}"
-  bgp_asn = "${var.leg_a_bgp_asn}"
-  destination_cidr_blocks = []
-  route_table_ids = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
-  route_table_count = "${length(concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids))}"
-  static_routes_only = "false"
+  name                        = "${var.name_prefix}-${var.leg_a_name}"
+  vpn_gateway_id              = "${module.vpn.vgw_id}"
+  ip_address                  = "${var.leg_a_ip_address}"
+  bgp_asn                     = "${var.leg_a_bgp_asn}"
+  destination_cidr_blocks     = []
+  route_table_ids             = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
+  route_table_count           = "${length(concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids))}"
+  static_routes_only          = "false"
   add_static_routes_to_tables = "false"
 }
 
 module "leg_b_customer_gateway" {
   source = "github.com/mergermarket/tf_aws_customer_gw"
 
-  name = "${var.name_prefix}-${var.leg_b_name}"
-  vpn_gateway_id = "${module.vpn.vgw_id}"
-  ip_address = "${var.leg_b_ip_address}"
-  bgp_asn = "${var.leg_b_bgp_asn}"
-  destination_cidr_blocks = []
-  route_table_ids = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
-  route_table_count = "${length(concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids))}"
-  static_routes_only = "false"
+  name                        = "${var.name_prefix}-${var.leg_b_name}"
+  vpn_gateway_id              = "${module.vpn.vgw_id}"
+  ip_address                  = "${var.leg_b_ip_address}"
+  bgp_asn                     = "${var.leg_b_bgp_asn}"
+  destination_cidr_blocks     = []
+  route_table_ids             = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
+  route_table_count           = "${length(concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids))}"
+  static_routes_only          = "false"
   add_static_routes_to_tables = "false"
 }
--- variables.tf
+++ variables.tf
@@ -3,46 +3,46 @@ variable "name_prefix" {
 }
 
 variable "vpc_cidr" {
-  type = "list"
+  type        = "list"
   description = "CIDR range for the VPC."
 }
 
 variable "private_subnet_cidrs" {
-  type = "list"
+  type        = "list"
   description = "CIDR ranges for the private subnets."
 }
 
 variable "public_subnet_cidrs" {
-  type = "list"
+  type        = "list"
   description = "CIDR ranges for the public subnets."
 }
 
 variable "leg_a_name" {
-  type = "string"
+  type        = "string"
   description = "Name label for leg A."
 }
 
 variable "leg_b_name" {
-  type = "string"
+  type        = "string"
   description = "Name label for leg B."
 }
 
 variable "leg_a_ip_address" {
-  type = "string"
+  type        = "string"
   description = "Endpoint on leg A firewall to establish a connection to."
 }
 
 variable "leg_b_ip_address" {
-  type = "string"
+  type        = "string"
   description = "Endpoint on leg b firewall to establish a connection to."
 }
 
 variable "leg_a_bgp_asn" {
-  type = "string"
+  type        = "string"
   description = "BGP Autonomous System Number for leg A. If BGP is not in use, then by convention set this value to 65000."
 }
 
 variable "leg_b_bgp_asn" {
-  type = "string"
+  type        = "string"
   description = "BGP Autonomous System Number for leg B. If BGP is not in use, then by convention set this value to 65000."
 }
