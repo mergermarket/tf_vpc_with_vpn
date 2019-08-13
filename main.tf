a36522eda274e2b4710ce42bb9ec2810a745d9ed
Keir Badger <keir.badger@acuris.com>
2019-08-13 13:32:23 +0100

    Add static routes and turn off route propagation

--- main.tf
+++ main.tf
@@ -45,11 +45,24 @@ module "leg_a_customer_gateway" {
   vpn_gateway_id              = "${module.vpn.vgw_id}"
   ip_address                  = "${var.leg_a_ip_address}"
   bgp_asn                     = "${var.leg_a_bgp_asn}"
-  destination_cidr_blocks     = []
+  destination_cidr_blocks     = [
+            "10.2.0.0/24",
+            "10.3.0.152/32",
+            "10.12.0.0/24",
+            "10.151.0.0/16",
+            "10.151.112.0/24",
+            "10.151.132.0/22",
+            "10.151.133.6/32",
+            "10.151.133.104/29",
+            "10.151.200.0/22",
+            "10.151.214.0/24",
+            "10.158.0.0/16",
+            "193.132.119.0/24",
+        ]
   route_table_ids             = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
   route_table_count           = "${var.route_table_count}"
-  static_routes_only          = "false"
-  add_static_routes_to_tables = "false"
+  static_routes_only          = "true"
+  add_static_routes_to_tables = "true"
 }
 
 module "leg_b_customer_gateway" {
@@ -59,9 +72,22 @@ module "leg_b_customer_gateway" {
   vpn_gateway_id              = "${module.vpn.vgw_id}"
   ip_address                  = "${var.leg_b_ip_address}"
   bgp_asn                     = "${var.leg_b_bgp_asn}"
-  destination_cidr_blocks     = []
+  destination_cidr_blocks     = [
+            "10.2.0.0/24",
+            "10.3.0.152/32",
+            "10.12.0.0/24",
+            "10.151.0.0/16",
+            "10.151.112.0/24",
+            "10.151.132.0/22",
+            "10.151.133.6/32",
+            "10.151.133.104/29",
+            "10.151.200.0/22",
+            "10.151.214.0/24",
+            "10.158.0.0/16",
+            "193.132.119.0/24",
+        ]
   route_table_ids             = ["${concat(module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)}"]
   route_table_count           = "${var.route_table_count}"
-  static_routes_only          = "false"
-  add_static_routes_to_tables = "false"
+  static_routes_only          = "true"
+  add_static_routes_to_tables = "true"
 }
